c04f76145016e6668d3a5e8177ba85124cbc6300
Spencer Whyte <spencerwhyte@Spencers-MacBook-Pro.local>
2014-07-22 17:57:17 -0400

    Initial Commit

---
+++ LaserPool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LaserPool
+//
+//  Created by Spencer Whyte on 2014-07-22.
+//  Copyright (c) 2014 Spencer Whyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
